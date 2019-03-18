cfcffd06db108f8009ae4f31493e0064b165e20f
Bruce Bigirwenkya <bruce.bigirwenkya@andela.com>
2019-03-18 10:52:07 +0300

    Add delete options for address endpoint

--- tests/api/v1/test_address_endpoint.py
+++ tests/api/v1/test_address_endpoint.py
@@ -70,34 +70,34 @@ class TestAddressEndpoint(BaseCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(result.get('address_line_1'), update_fields.get('address1'))
 
-    # def test_delete_removes_address_from_database(self):
-    #     with self.app.app_context():
+    def test_delete_removes_address_from_database(self):
+        with self.app.app_context():
 
-    #         self.assertEqual(len(Address.query.filter_by(active=True).all()), 2)
+            self.assertEqual(len(Address.query.filter_by(active=True).all()), 2)
 
-    #         response = self.client().delete('/api/v1/addresses/1',
-    #                                         headers=self.auth_headers())
-    #     result = json.loads(response.data.decode('utf-8'))
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertEqual(result.get('message'), 'Address with id 1 successfully deleted.')
-    #     with self.app.app_context():
-    #         self.assertEqual(len(Address.query.filter_by(active=True).all()), 1)
+            response = self.client().delete('/api/v1/addresses/1',
+                                            headers=self.auth_headers())
+        result = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(result.get('message'), 'Address with id 1 successfully deleted.')
+        with self.app.app_context():
+            self.assertEqual(len(Address.query.filter_by(active=True).all()), 1)
 
-    # def test_search_returns_addresses_whose_name_matches_a_search_term(self):
-    #     with self.app.app_context():
-    #         response = self.client().get('/api/v1/addresses?q=sample',
-    #                                      headers=self.auth_headers())
-    #     result = json.loads(response.data.decode('utf-8')).get('data')
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertEqual(len(result), 2)
+    def test_search_returns_addresses_whose_address_includes_the_search_term(self):
+        with self.app.app_context():
+            response = self.client().get('/api/v1/addresses?q=TRM Drive',
+                                         headers=self.auth_headers())
+        result = json.loads(response.data.decode('utf-8')).get('data')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(result), 1)
 
-    # def test_pagination_of_addresses_when_you_pass_a_limit_parameter(self):
-    #     with self.app.app_context():
-    #         response = self.client().get('/api/v1/addresses?limit=1',
-    #                                      headers=self.auth_headers())
-    #     result = json.loads(response.data.decode('utf-8'))
+    def test_pagination_of_addresses_when_you_pass_a_limit_parameter(self):
+        with self.app.app_context():
+            response = self.client().get('/api/v1/addresses?limit=1',
+                                         headers=self.auth_headers())
+        result = json.loads(response.data.decode('utf-8'))
 
-    #     self.assertEqual(response.status_code, 200)
-    #     expected_result = sorted(['data', 'next_page','page', 'per_page', 'total_data', 'pages', 'prev_page'])
-    #     self.assertListEqual(sorted(result.keys()), expected_result)
-    #     self.assertEqual(len(result.get('data')), 1)
+        self.assertEqual(response.status_code, 200)
+        expected_result = sorted(['data', 'next_page','page', 'per_page', 'total_data', 'pages', 'prev_page'])
+        self.assertListEqual(sorted(result.keys()), expected_result)
+        self.assertEqual(len(result.get('data')), 1)
